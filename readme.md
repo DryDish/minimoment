3ba831b16b886183b46ad1c7a412233224a0cd7c
DryDish <57399961+DryDish@users.noreply.github.com>
2022-03-19 15:27:54 +0100

    add current files and readme to repo

---
+++ readme.md
@@ -0,0 +1,52 @@
+Studends: Alex, Cristian and Teodor.
+
+index:  ❌ = Missing, ☑️ = Done
+
+# Mandatory assignment part 1
+
+Start with the implementation of the final project. 
+The task for the assignment 1 is to design a relational database of your choice(MySQL is recommended)
+for the project according to the requirements described in the final project description.
+
+
+**The goal is to:**
+
+* Create the ERD for your relational database(conceptual, logical, physical). ☑️
+* Generate the database to a database server (for example MySQL server). ☑️
+* Make sure your design is working – primary and foreign keys, data types, constraints, referential integrity checks, ... ☑️
+* Write SQLscripts for populating your database with the test data. ☑️
+* Implement: 
+    * stored procedures ☑️
+    * stored functions ☑️
+    * indexes for faster querying ☑️
+    * views ☑️
+    * triggers ❌
+    * events ☑️
+    * Write a report which is described below. ❌
+
+**Delivery**
+
+Hand in on Fronter before the deadline (25-03-2022).
+
+**Artifacts**
+
+* Relational database scripts (one or several), including:
+    * Database creation, including tables, keys, indexes, constraints, and referential integrity checks. ☑️
+    * Load of test data ☑️
+    * Stored procedures ☑️
+    * Trigger ❌
+    * Views ☑️
+    * Events ☑️
+    * A brief installation procedure that specifies how to import the database ina test environment with full operational capabilities ❌
+
+
+**Report**
+
+Follow the structure required for the final report and just pick the parts that are covered by the mandatory assignment 1.
+
+It is fine if some parts are not completely finished as the project will continue. In that case you can describe your ideas about how you are going to do the missing parts later.
+
+
+# Mandatory assignment part 2
+
+- TBD
---
+++ sql/scripts/creation-script.sql
@@ -0,0 +1,391 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema store
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `store` ;
+
+-- -----------------------------------------------------
+-- Schema store
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `store` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ;
+USE `store` ;
+
+-- -----------------------------------------------------
+-- Table `contact_info`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `contact_info` ;
+
+CREATE TABLE IF NOT EXISTS `contact_info` (
+  `contact_info_id` INT NOT NULL AUTO_INCREMENT,
+  `phone_number` VARCHAR(20) NOT NULL,
+  `country_code` VARCHAR(3) NOT NULL,
+  `city` VARCHAR(97) NOT NULL,
+  `postal_code` VARCHAR(8) NOT NULL,
+  `address_one` VARCHAR(255) NOT NULL,
+  `address_two` VARCHAR(255) NULL,
+  PRIMARY KEY (`contact_info_id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `roles`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `roles` ;
+
+CREATE TABLE IF NOT EXISTS `roles` (
+  `roles_id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`roles_id`))
+ENGINE = InnoDB;
+
+CREATE UNIQUE INDEX `name_UNIQUE` ON `roles` (`name` ASC) VISIBLE;
+
+
+-- -----------------------------------------------------
+-- Table `users`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `users` ;
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` INT NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(60) NULL,
+  `last_name` VARCHAR(60) NULL,
+  `username` VARCHAR(255) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  `contact_info_id` INT NULL,
+  `roles_id` INT NOT NULL,
+  `auto_renew` TINYINT(1) NULL,
+  PRIMARY KEY (`user_id`),
+  CONSTRAINT `fk_user_contact_info`
+    FOREIGN KEY (`contact_info_id`)
+    REFERENCES `contact_info` (`contact_info_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_users_roles`
+    FOREIGN KEY (`roles_id`)
+    REFERENCES `roles` (`roles_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE INDEX `fk_user_contact_info_idx` ON `users` (`contact_info_id` ASC) VISIBLE;
+
+CREATE INDEX `fk_users_roles_idx` ON `users` (`roles_id` ASC) VISIBLE;
+
+CREATE UNIQUE INDEX `username_UNIQUE` ON `users` (`username` ASC) VISIBLE;
+
+
+-- -----------------------------------------------------
+-- Table `subscription_types`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `subscription_types` ;
+
+CREATE TABLE IF NOT EXISTS `subscription_types` (
+  `subscription_type_id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `monthly_price` FLOAT NOT NULL,
+  `image_amount` INT NOT NULL,
+  PRIMARY KEY (`subscription_type_id`))
+ENGINE = InnoDB;
+
+CREATE UNIQUE INDEX `name_UNIQUE` ON `subscription_types` (`name` ASC) VISIBLE;
+
+
+-- -----------------------------------------------------
+-- Table `discount_types`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `discount_types` ;
+
+CREATE TABLE IF NOT EXISTS `discount_types` (
+  `discount_type_id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`discount_type_id`))
+ENGINE = InnoDB;
+
+CREATE UNIQUE INDEX `name_UNIQUE` ON `discount_types` (`name` ASC) VISIBLE;
+
+
+-- -----------------------------------------------------
+-- Table `discount_codes`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `discount_codes` ;
+
+CREATE TABLE IF NOT EXISTS `discount_codes` (
+  `discount_code_id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `discount_type_id` INT NOT NULL,
+  `value` FLOAT NOT NULL,
+  `valid_from` DATETIME NOT NULL,
+  `valid_to` DATETIME NOT NULL,
+  `remaining_uses` INT NOT NULL,
+  PRIMARY KEY (`discount_code_id`),
+  CONSTRAINT `fk_discount_codes_discount_type`
+    FOREIGN KEY (`discount_type_id`)
+    REFERENCES `discount_types` (`discount_type_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE INDEX `fk_discount_codes_discount_type_idx` ON `discount_codes` (`discount_type_id` ASC) VISIBLE;
+
+CREATE UNIQUE INDEX `name_UNIQUE` ON `discount_codes` (`name` ASC) VISIBLE;
+
+
+-- -----------------------------------------------------
+-- Table `statuses`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `statuses` ;
+
+CREATE TABLE IF NOT EXISTS `statuses` (
+  `status_id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`status_id`))
+ENGINE = InnoDB;
+
+CREATE UNIQUE INDEX `name_UNIQUE` ON `statuses` (`name` ASC) VISIBLE;
+
+
+-- -----------------------------------------------------
+-- Table `orders`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `orders` ;
+
+CREATE TABLE IF NOT EXISTS `orders` (
+  `order_id` INT NOT NULL AUTO_INCREMENT,
+  `discount_code_id` INT NOT NULL,
+  `user_id` INT NOT NULL,
+  `billing_contact_info_id` INT NOT NULL,
+  `status_id` INT NOT NULL,
+  `order_price` FLOAT NOT NULL,
+  `total_price_saved` FLOAT NULL,
+  `created_at` DATETIME NOT NULL,
+  PRIMARY KEY (`order_id`),
+  CONSTRAINT `fk_order_discount_codes`
+    FOREIGN KEY (`discount_code_id`)
+    REFERENCES `discount_codes` (`discount_code_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_orders_users`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `users` (`user_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_orders_contact_info`
+    FOREIGN KEY (`billing_contact_info_id`)
+    REFERENCES `contact_info` (`contact_info_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_orders_statuses`
+    FOREIGN KEY (`status_id`)
+    REFERENCES `statuses` (`status_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE INDEX `fk_order_discount_codes_idx` ON `orders` (`discount_code_id` ASC) VISIBLE;
+
+CREATE INDEX `fk_orders_users_idx` ON `orders` (`user_id` ASC) VISIBLE;
+
+CREATE INDEX `fk_orders_contact_info_idx` ON `orders` (`billing_contact_info_id` ASC) VISIBLE;
+
+CREATE INDEX `fk_orders_statuses_idx` ON `orders` (`status_id` ASC) VISIBLE;
+
+
+-- -----------------------------------------------------
+-- Table `invoice`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `invoice` ;
+
+CREATE TABLE IF NOT EXISTS `invoice` (
+  `invoice_id` INT NOT NULL AUTO_INCREMENT,
+  `order_id` INT NOT NULL,
+  `created_at` DATETIME NOT NULL,
+  PRIMARY KEY (`invoice_id`),
+  CONSTRAINT `fk_invoice_orders`
+    FOREIGN KEY (`order_id`)
+    REFERENCES `orders` (`order_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE INDEX `fk_invoice_orders_idx` ON `invoice` (`order_id` ASC) VISIBLE;
+
+
+-- -----------------------------------------------------
+-- Table `picture_data`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `picture_data` ;
+
+CREATE TABLE IF NOT EXISTS `picture_data` (
+  `picture_data_id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` INT NOT NULL,
+  `image_url` VARCHAR(255) NOT NULL,
+  `uploaded_at` DATETIME NOT NULL,
+  PRIMARY KEY (`picture_data_id`),
+  CONSTRAINT `fk_picture_data_users`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `users` (`user_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE INDEX `fk_picture_data_users_idx` ON `picture_data` (`user_id` ASC) VISIBLE;
+
+
+-- -----------------------------------------------------
+-- Table `sizes`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `sizes` ;
+
+CREATE TABLE IF NOT EXISTS `sizes` (
+  `size_id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `width_mm` INT NOT NULL,
+  `height_mm` INT NOT NULL,
+  `price` FLOAT NOT NULL,
+  PRIMARY KEY (`size_id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `frames`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `frames` ;
+
+CREATE TABLE IF NOT EXISTS `frames` (
+  `frame_id` INT NOT NULL AUTO_INCREMENT,
+  `discount_code_id` INT NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  `price` FLOAT NOT NULL,
+  `material` VARCHAR(45) NULL,
+  `sizes_size_id` INT NOT NULL,
+  PRIMARY KEY (`frame_id`),
+  CONSTRAINT `fk_frame_discount_codes`
+    FOREIGN KEY (`discount_code_id`)
+    REFERENCES `discount_codes` (`discount_code_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_frames_sizes`
+    FOREIGN KEY (`sizes_size_id`)
+    REFERENCES `sizes` (`size_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE INDEX `fk_frame_discount_codes_idx` ON `frames` (`discount_code_id` ASC) VISIBLE;
+
+CREATE INDEX `fk_frames_sizes_idx` ON `frames` (`sizes_size_id` ASC) VISIBLE;
+
+
+-- -----------------------------------------------------
+-- Table `paper_types`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `paper_types` ;
+
+CREATE TABLE IF NOT EXISTS `paper_types` (
+  `paper_type_id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  `multiplier` DECIMAL(3,2) NOT NULL,
+  `size_id` INT NOT NULL,
+  `discount_code_id` INT NOT NULL,
+  PRIMARY KEY (`paper_type_id`, `multiplier`),
+  CONSTRAINT `fk_paper_types_sizes`
+    FOREIGN KEY (`size_id`)
+    REFERENCES `sizes` (`size_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_paper_types_discount_codes`
+    FOREIGN KEY (`discount_code_id`)
+    REFERENCES `discount_codes` (`discount_code_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE INDEX `fk_paper_types_sizes_idx` ON `paper_types` (`size_id` ASC) VISIBLE;
+
+CREATE INDEX `fk_paper_types_discount_codes_idx` ON `paper_types` (`discount_code_id` ASC) VISIBLE;
+
+
+-- -----------------------------------------------------
+-- Table `order_items`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `order_items` ;
+
+CREATE TABLE IF NOT EXISTS `order_items` (
+  `order_item_id` INT NOT NULL AUTO_INCREMENT,
+  `picture_data_id` INT NULL,
+  `paper_type_id` INT NULL,
+  `frame_id` INT NULL,
+  `order_id` INT NOT NULL,
+  `order_item_price` FLOAT NOT NULL,
+  `price_saved` FLOAT NULL,
+  `amount` INT NOT NULL,
+  PRIMARY KEY (`order_item_id`),
+  CONSTRAINT `fk_order_item_picture_data`
+    FOREIGN KEY (`picture_data_id`)
+    REFERENCES `picture_data` (`picture_data_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_order_item_paper_type`
+    FOREIGN KEY (`paper_type_id`)
+    REFERENCES `paper_types` (`paper_type_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_order_item_frame`
+    FOREIGN KEY (`frame_id`)
+    REFERENCES `frames` (`frame_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_order_item_order`
+    FOREIGN KEY (`order_id`)
+    REFERENCES `orders` (`order_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE INDEX `fk_order_item_picture_data_idx` ON `order_items` (`picture_data_id` ASC) VISIBLE;
+
+CREATE INDEX `fk_order_item_paper_type_idx` ON `order_items` (`paper_type_id` ASC) VISIBLE;
+
+CREATE INDEX `fk_order_item_frame_idx` ON `order_items` (`frame_id` ASC) VISIBLE;
+
+CREATE INDEX `fk_order_item_order_idx` ON `order_items` (`order_id` ASC) VISIBLE;
+
+
+-- -----------------------------------------------------
+-- Table `subscriptions`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `subscriptions` ;
+
+CREATE TABLE IF NOT EXISTS `subscriptions` (
+  `subscription_id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` INT NOT NULL,
+  `subscription_type_id` INT NOT NULL,
+  `starts_at` DATETIME NOT NULL,
+  `ends_at` DATETIME NOT NULL,
+  PRIMARY KEY (`subscription_id`),
+  CONSTRAINT `fk_subscription_subscription_type`
+    FOREIGN KEY (`subscription_type_id`)
+    REFERENCES `subscription_types` (`subscription_type_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_subscription_users`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `users` (`user_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE INDEX `fk_subscription_subscription_type_idx` ON `subscriptions` (`subscription_type_id` ASC) VISIBLE;
+
+CREATE INDEX `fk_subscription_users_idx` ON `subscriptions` (`user_id` ASC) VISIBLE;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
