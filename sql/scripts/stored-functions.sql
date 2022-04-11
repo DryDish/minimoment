e7b54c878348f911c06e29b862773819fec6b9b3
CristiPV <cristi.valentin29@gmail.com>
2022-04-11 16:17:09 +0200

    add calculate discount stored function

--- sql/scripts/stored-functions.sql
+++ sql/scripts/stored-functions.sql
@@ -53,3 +53,34 @@ DELIMITER ;
 -- SELECT fn_calculate_order_item_price(10, 1.5, 20, 1.2, 10);
 -- SELECT fn_calculate_order_item_price(10, 1.5, 20, null, 10);
 -- SELECT fn_calculate_order_item_price(10, null, 20, 1.2, 10);
+
+
+-- ----------------------------------------------------------------------------------------------------------------------------------------
+-- Function `fn_calculate_discount` -> Calculates the discount of a price.
+-- ----------------------------------------------------------------------------------------------------------------------------------------
+DELIMITER $$
+DROP FUNCTION IF EXISTS `fn_calculate_discount`$$
+CREATE FUNCTION `fn_calculate_discount`(
+	`discount_value` DECIMAL(15, 2),
+    `discount_type` VARCHAR(45),
+    `price` DECIMAL(15, 2),
+    `amount` INT
+	)
+RETURNS DECIMAL(15,2)
+DETERMINISTIC
+BEGIN
+	SET `amount` = IFNULL(`amount`, 1);
+	SET `price` = IF(`discount_type` = "percent", IFNULL(`price`, 1), 1);
+    
+	RETURN `discount_value` * `price` * `amount`;
+END $$
+DELIMITER ;
+
+-- Example usages
+-- SELECT fn_calculate_discount(100, "amount", null, null);  -- Expected: 100
+-- SELECT fn_calculate_discount(100, "amount", 400, null);   -- Expected: 100
+-- SELECT fn_calculate_discount(100, "amount", 400, 1);      -- Expected: 100
+
+-- SELECT fn_calculate_discount(0.1, "percent", null, null); -- Expected: 0.1
+-- SELECT fn_calculate_discount(0.1, "percent", 100, null);  -- Expected: 10
+-- SELECT fn_calculate_discount(0.1, "percent", 100, 5);     -- Expected: 50
