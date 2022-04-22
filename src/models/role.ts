c25c9ff6d5c06cbe992c672073e5a90390189e1f
CristiPV <cristi.valentin29@gmail.com>
2022-04-22 18:31:30 +0200

    format files

--- src/models/role.ts
+++ src/models/role.ts
@@ -2,22 +2,23 @@ import { DataTypes, Model, Sequelize } from "sequelize";
 
 class Role extends Model {}
 
-export default (sequelize: Sequelize) => Role.init(
-  {
-    role_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
+export default (sequelize: Sequelize) =>
+  Role.init(
+    {
+      role_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING(45),
+        allowNull: false,
+      },
     },
-    name: {
-      type: DataTypes.STRING(45),
-      allowNull: false,
-    },
-  },
-  {
-    sequelize,
-    modelName: "Role",
-    tableName: "Roles"
-  }
-);
+    {
+      sequelize,
+      modelName: "Role",
+      tableName: "Roles",
+    }
+  );
