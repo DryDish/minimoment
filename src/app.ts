8507f3e6c50b8027c9ca0981752ef4120143c00d
CristiPV <cristi.valentin29@gmail.com>
2022-04-27 15:03:45 +0200

    add login functionality

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import "dotenv/config";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import express from "express";
-import {
-  authenticate as mysqlConnect,
-} from "./services/sequelize.service";
+import { authenticate as mysqlConnect } from "./services/sequelize.service";
 
 // Import middleware
 import AuthMiddleware from "./middleware/authenticate.middleware";
@@ -12,6 +12,7 @@ import RolesRouter from "./routes/roles.routes";
 
 // Constants
 const SERVER_PORT = process.env.SERVER_PORT || 5000;
+const SECRET_KEY = process.env.AUTH_SECRET_KEY || "";
 
 // Connect to the database
 mysqlConnect();
@@ -29,6 +30,36 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send({ response: "Hello World!" });
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  // TODO: Find user by username
+
+  // TODO: replace userPassword with the real User's hashed password
+  const userPassword = await bcrypt.hash(password, 10);
+
+  bcrypt.compare(password, userPassword, (error, same) => {
+    if (error) {
+      res.status(401).send("Unauthorized");
+    } else if (!same) {
+      res.status(401).send("Unauthorized");
+    } else {
+      jwt.sign(
+        { user: username },
+        SECRET_KEY,
+        { expiresIn: "24h" },
+        (error: Error | null, token: string | undefined) => {
+          if (error) res.sendStatus(500);
+          else
+            res.send({
+              user: { username },
+              token,
+            });
+        }
+      );
+    }
+  });
 });
 
 app.use(AuthMiddleware);
