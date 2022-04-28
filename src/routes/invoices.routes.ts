5591b7b6329ad74696aae0d87a6262f00c8134fd
Teodor_Jonasson <teodor_jonasson@hotmail.com>
2022-04-28 15:41:32 +0200

    feat: 🎸 Routes for invoice

--- src/routes/invoices.routes.ts
+++ src/routes/invoices.routes.ts
@@ -10,3 +10,66 @@ router.get("/", async (_, res) => {
   
     res.send({ invoices: result });
 });
+
+router.get("/:invoice_id", async (req, res) => {
+    const { invoice_id } = req.params;
+    const result = await invoices.findByPk(invoice_id);
+  
+    if (result) {
+      res.send({ invoice: result });
+    } else {
+      res.status(404).send({ error: 404, message: "Invoice not found." });
+    }
+});
+
+router.post("/",async (req, res) => {
+    const {
+        orderId,
+        createdAt,
+    } = req.body;
+
+    const invoice = Invoice.build({
+        orderId,
+        createdAt,
+    });
+
+    const result = await invoice.save();
+
+    res.status(201).send({ invoice: result });
+});
+
+router.patch("/:invoice_id", async (req, res) => {
+    const { invoice_id } = req.params;
+    const {
+        orderId,
+        createdAt,
+    } = req.body;
+  
+    const invoiceToEdit = await invoices.findByPk(invoice_id);
+  
+    if (invoiceToEdit) {
+        const result = await invoiceToEdit.update({
+            orderId,
+            createdAt,
+        });
+  
+        res.send({ invoice: result });
+    } else {
+        res.status(404).send({ error: 404, message: "invoice not found." });
+    }
+});
+
+router.delete("/:invoice_id", async (req, res) => {
+    const { invoice_id } = req.params;
+    const invoiceToEdit = await invoices.findByPk(invoice_id);
+
+    if (invoiceToEdit) {
+    await invoiceToEdit.destroy();
+
+    res.send({ message: "Success!" });
+    } else {
+    res.status(404).send({ error: 404, message: "Invoice not found." });
+    }
+});
+
+export default router;
