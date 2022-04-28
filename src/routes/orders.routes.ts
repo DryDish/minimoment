26d7111890fab3ec967e8866bbdf877e79a68f02
Teodor_Jonasson <teodor_jonasson@hotmail.com>
2022-04-28 12:32:35 +0200

    feat: 🎸 add findAll findByPk and post for order

--- src/routes/orders.routes.ts
+++ src/routes/orders.routes.ts
@@ -8,6 +8,44 @@ const orders = sequelize.models.Order;
 router.get("/",async (_, res) => {
     const result = await orders.findAll();
 
-    res.send({users: result});
+    res.send({ orders: result });
 });
+
+router.get("/:id", async (req, res) => {
+    const { id } = req.params;
+    const result = await orders.findByPk(id);
+
+    if (result) {
+        res.send({ order: result });
+    } else {
+        res.status(404).send({ error: 404, message: "Order not found." });
+    }
+});
+
+router.post("/",async (req, res) => {
+    const {
+        discountCodeId,
+        userId,
+        billingContactInfoId,
+        statusId,
+        orderPrice,
+        totalPriceSaved,
+        createdAt,
+    } = req.body;
+
+    const order = Order.build({
+        discountCodeId,
+        userId,
+        billingContactInfoId,
+        statusId,
+        orderPrice,
+        totalPriceSaved,
+        createdAt,
+    });
+
+    const result = await order.save();
+
+    res.status(201).send({ order: result });
+});
+
 
