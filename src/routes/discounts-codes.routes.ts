d586f97c8fb1e7c4ea4e277ef87547f36c5f5258
DryDish <57399961+DryDish@users.noreply.github.com>
2022-04-28 16:08:01 +0200

    finish crud implementation for discount-code routes

--- src/routes/discounts-codes.routes.ts
+++ src/routes/discounts-codes.routes.ts
@@ -1,13 +1,13 @@
 import express from "express";
 import { DiscountCode } from "../models/discount-code";
 
-const router = express.Router();
+export const router = express.Router();
 
 router.get("/", async (_, res) => {
   try {
     const discountCodes = await DiscountCode.findAll();
 
-    res.status(200).json({ discountCodes: discountCodes });
+    res.status(200).json({ discountCodes });
   } catch (error) {
     console.log(error);
 
@@ -17,5 +17,112 @@ router.get("/", async (_, res) => {
     });
   }
 });
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const discountCode = await DiscountCode.findByPk(id);
+
+    if (discountCode === null) {
+      res.status(404).send({ error: 404, message: "Not found." });
+    } else {
+      res.status(200).send({ discountCode });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({
+      error: 500,
+      message: "Unable to retrieve the discount code.",
+    });
+  }
+});
+
+router.post("/", async (req, res) => {
+  const requestObject = filterBody(req.body);
+
+  const discountCode = DiscountCode.build(requestObject);
+
+  try {
+    await discountCode.save();
+
+    res.status(200).send({ discountCode });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).send({
+      error: 500,
+      message: "Unable to save new discount.",
+    });
+  }
+});
+
+router.patch("/:id", async (req, res) => {
+  const { id } = req.params;
+  const requestObject = filterBody(req.body);
+
+  const discountCode = await DiscountCode.findByPk(id);
+  if (!discountCode) {
+    res.status(404).send({ error: 404, message: "Not found." });
+    return;
+  }
+
+  try {
+    await discountCode.update(requestObject);
+
+    res.status(200).send({ discountCode });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).send({
+      error: 500,
+      message: "Unable to save new discount.",
+    });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const discountCode = await DiscountCode.findByPk(id);
+
+  if (!discountCode) {
+    res.status(404).send({ error: 404, message: "Not found." });
+    return;
+  }
+
+  try {
+    await discountCode.destroy();
+    res.status(200).send({ deleted: discountCode });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).send({
+      error: 500,
+      message: "Unable to delete the discount type.",
+    });
+  }
+});
+
+const filterBody = (body: {
+  name: any;
+  value: any;
+  validFrom: any;
+  validTo: any;
+  remainingUses: any;
+  discountCodeId: any;
+}) => {
+  const { name, value, validFrom, validTo, remainingUses, discountCodeId } =
+    body;
+
+  return {
+    name,
+    value,
+    validFrom,
+    validTo,
+    remainingUses,
+    discountCodeId,
+  };
+};
 
 export default router;
