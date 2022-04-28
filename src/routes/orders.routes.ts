45f1ffa548729d8087fec73134e8facb774b7a71
Teodor_Jonasson <teodor_jonasson@hotmail.com>
2022-04-28 12:36:48 +0200

    feat: 🎸 Create update method for order

--- src/routes/orders.routes.ts
+++ src/routes/orders.routes.ts
@@ -48,4 +48,33 @@ router.post("/",async (req, res) => {
     res.status(201).send({ order: result });
 });
 
+router.patch("/:id",async (req, res) => {
+    const { id } = req.params;
+    const {
+        discountCodeId,
+        userId,
+        billingContactInfoId,
+        statusId,
+        orderPrice,
+        totalPriceSaved,
+        createdAt,
+    } = req.body;
 
+    const orderToEdit = await orders.findByPk(id);
+
+    if (orderToEdit) {
+        const result = await orderToEdit.update({
+            discountCodeId,
+            userId,
+            billingContactInfoId,
+            statusId,
+            orderPrice,
+            totalPriceSaved,
+            createdAt,
+        });
+
+        res.send({ result: result });
+    } else {
+        res.status(404).send({ error: 404, message: "Order not found." });
+    }
+})
