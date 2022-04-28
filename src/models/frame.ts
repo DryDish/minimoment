4408da52ddcf1e661da967f30f8cac16c5035195
Teodor_Jonasson <teodor_jonasson@hotmail.com>
2022-04-28 14:38:42 +0200

    feat: 🎸 Add frame model

---
+++ src/models/frame.ts
@@ -0,0 +1,46 @@
+import { DataTypes, Model, Sequelize } from "sequelize";
+
+export class Frame extends Model {}; 
+
+export default (sequelize: Sequelize) => {
+    Frame.init(
+        {
+            frameId: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                field: "frame_id",
+            },
+            discountCodeId: {
+                type: DataTypes.INTEGER,
+                allowNull: true,
+                field: "discount_code_id"
+            },
+            name: {
+                type: DataTypes.STRING(45),
+                allowNull: false,
+                field: "name",
+            },
+            multiplier: {
+                type: DataTypes.DECIMAL(3, 2),
+                allowNull: true,
+                field: "multiplier",
+            },
+            material: {
+                type: DataTypes.STRING(45),
+                allowNull: false,
+                field: "material",
+            },
+            sizeId: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                field: "size_id",
+            }
+        },
+        {
+            sequelize,
+            tableName: "frames",
+        }
+    )
+}
