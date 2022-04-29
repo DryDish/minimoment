584a95d0b003aebd29508962a8db09a0ef2fea46
Teodor_Jonasson <teodor_jonasson@hotmail.com>
2022-04-29 10:38:02 +0200

    feat: 🎸 Add POST PATCH and DELETE endpoints to order item rout

--- src/routes/order-items.routes.ts
+++ src/routes/order-items.routes.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { OrderItem } from "../models/order-item";
 import { sequelize } from "../services/sequelize.service";
 
 const router = express.Router();
@@ -56,6 +57,90 @@ router.get("/by-user/:user_id", async (req, res) => {
         res.send({ orderItems: orderItemList });
     } else {
         res.status(404).send({ error: 404, message: "Order items not found." });
+    }
+});
+
+// POST create an order_item
+router.post("/", async (req, res) => {
+    const {
+        pictureDataId,
+        paperTypeId,
+        frameId,
+        orderId,
+        orderItemPrice,
+        priceSaved,
+        amount,
+    } = req.body;
+
+    const orderItem = OrderItem.build({
+        pictureDataId,
+        paperTypeId,
+        frameId,
+        orderId,
+        orderItemPrice,
+        priceSaved,
+        amount,
+    });
+
+    const result = await orderItem.save().catch((error) => {
+        console.log(error);
+    });
+
+    res.status(201).send({ orderItem: result });
+});
+
+// PATCH update an order_item
+router.patch("/:order_item_id", async (req, res) => {
+    const { order_item_id } = req.params;
+    const {
+        pictureDataId,
+        paperTypeId,
+        frameId,
+        orderId,
+        orderItemPrice,
+        priceSaved,
+        amount,
+    } = req.body;
+
+    const orderItemToEdit = await orderItems.findByPk(order_item_id).catch((error) => {
+        console.log(error);
+    });
+
+    if (orderItemToEdit) {
+        const result = await orderItemToEdit.update({
+            pictureDataId,
+            paperTypeId,
+            frameId,
+            orderId,
+            orderItemPrice,
+            priceSaved,
+            amount,
+        }).catch((error) => {
+            console.log(error);
+        });
+
+        res.send({ order: result });
+    } else {
+        res.status(404).send({ error: 404, message: "Order item not found." });
+    }
+});
+
+// DELETE an order_item
+router.delete("/:order_item_id", async (req, res) => {
+    const { order_item_id } = req.params;
+    
+    const orderItemToDelete = await orderItems.findByPk(order_item_id).catch((error) => {
+        console.log(error);
+    });
+
+    if (orderItemToDelete) {
+        await orderItemToDelete.destroy().catch((error) => {
+            console.log(error);
+        });
+
+        res.status(201).send({ message: "success" });
+    } else {
+        res.status(404).send({ error: 404, message: "Order item not found." });
     }
 });
 
