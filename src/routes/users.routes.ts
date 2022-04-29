3db0faf7ddfb9b850d464e31ab1a23ad845284ad
DryDish <57399961+DryDish@users.noreply.github.com>
2022-04-29 12:55:23 +0200

    update user/getById

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -5,8 +5,6 @@ import { User } from "../models/user";
 const router = express.Router();
 const users = sequelize.models.User;
 
-// TODO: add try-catch in case of database errors
-
 router.get("/", async (_, res) => {
   try {
     const result = await users.findAll();
@@ -24,12 +22,22 @@ router.get("/", async (_, res) => {
 
 router.get("/:id", async (req, res) => {
   const { id } = req.params;
-  const result = await users.findByPk(id);
 
-  if (result) {
-    res.send({ user: result });
-  } else {
-    res.status(404).send({ error: 404, message: "User not found." });
+  try {
+    const result = await users.findByPk(id);
+
+    if (result) {
+      res.status(200).send(result);
+    } else {
+      res.status(404).send({ error: 404, message: "User not found." });
+    }
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).send({
+      error: 500,
+      message: "Unable to retrieve user account.",
+    });
   }
 });
 
