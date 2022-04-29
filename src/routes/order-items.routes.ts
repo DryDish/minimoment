a1f81529abbde9f11ed6b72603cbc39b99db178d
DryDish <57399961+DryDish@users.noreply.github.com>
2022-04-29 21:34:53 +0200

    finish updating order-items routes

--- src/routes/order-items.routes.ts
+++ src/routes/order-items.routes.ts
@@ -1,147 +1,147 @@
 import express from "express";
+import { Order } from "../models/order";
 import { OrderItem } from "../models/order-item";
-import { sequelize } from "../services/sequelize.service";
+import { sendErrorResponse } from "../utils/responses.util";
 
 const router = express.Router();
-const orderItems = sequelize.models.OrderItem;
-
-// GET a list of order_items by order_id
-router.get("/by-order/:order_id", async (req, res) => {
-    const { order_id } = req.params;
-
-    const orderItemList = await orderItems.findAndCountAll({
-        where: {
-            orderId: order_id,
-        },
-    }).catch((error) => {
-        console.log(error);
-    });
-
-    // This returns an object that looks like this => orderItems: { count: 2, rows: [ orderItem, orderItem ]}
-    if (orderItemList) {
-        res.send({ orderItems: orderItemList });
-    } else {
-        res.status(404).send({ error: 404, message: "Order items not found." });
-    }
-});
 
 // GET single order_item by order_item_id
-router.get("/:order_item_id", async (req, res) => {
-    const { order_item_id } = req.params;
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
 
-    const result = await orderItems.findByPk(order_item_id).catch((error) => {
-        console.log(error);
-    });
-
+  try {
+    const result = await OrderItem.findByPk(id);
     if (result) {
-        res.status(201).send({ orderItem: result });
+      res.status(201).send(result);
     } else {
-        res.status(404).send({ error: 404, message: "Order items not found." });
+      sendErrorResponse(res, "Order item not found.", 404);
     }
+  } catch (error) {
+    sendErrorResponse(res, "Unable to retrieve order item.", 500, error);
+  }
+});
+
+// GET a list of order_items by order_id
+router.get("/by-order/:orderId", async (req, res) => {
+  const { orderId } = req.params;
+
+  try {
+    const orderItemList = await OrderItem.findAll({
+      where: { orderId },
+    });
+    res.status(200).send(orderItemList);
+  } catch (error) {
+    sendErrorResponse(
+      res,
+      "Unable to retrieve order items by order id.",
+      500,
+      error
+    );
+  }
 });
 
 // GET a list of order_items by user_id
-router.get("/by-user/:user_id", async (req, res) => {
-    const { user_id } = req.params;
+router.get("/by-user/:userId", async (req, res) => {
+  const { userId } = req.params;
 
-    const orderItemList = await orderItems.findAndCountAll({
-        where: {
-            orderId: user_id,
-        },
-    }).catch((error) => {
-        console.log(error);
-    });
+  try {
+    const orders = await Order.findAll({ where: { userId } });
+    const orderItems: OrderItem[] = [];
 
-    // This returns an object that looks like this => orderItems: { count: 2, rows: [ orderItem, orderItem ]}
-    if (orderItemList) {
-        res.send({ orderItems: orderItemList });
-    } else {
-        res.status(404).send({ error: 404, message: "Order items not found." });
+    for (const order of orders) {
+      orderItems.push(
+        ...(await OrderItem.findAll({
+          where: { orderId: order.getDataValue("orderId") },
+        }))
+      );
     }
+    res.status(200).send(orderItems);
+  } catch (error) {
+    sendErrorResponse(
+      res,
+      "Unable to retrieve order items by user id.",
+      500,
+      error
+    );
+  }
 });
 
 // POST create an order_item
 router.post("/", async (req, res) => {
-    const {
-        pictureDataId,
-        paperTypeId,
-        frameId,
-        orderId,
-        orderItemPrice,
-        priceSaved,
-        amount,
-    } = req.body;
+  const requestBody = filterBody(req.body);
 
-    const orderItem = OrderItem.build({
-        pictureDataId,
-        paperTypeId,
-        frameId,
-        orderId,
-        orderItemPrice,
-        priceSaved,
-        amount,
-    });
-
-    const result = await orderItem.save().catch((error) => {
-        console.log(error);
-    });
-
-    res.status(201).send({ orderItem: result });
+  const orderItem = OrderItem.build(requestBody);
+  try {
+    const savedOrderItem = await orderItem.save();
+    res.status(201).send(savedOrderItem);
+  } catch (error) {
+    sendErrorResponse(res, "Unable to save order item.", 500, error);
+  }
 });
 
 // PATCH update an order_item
 router.patch("/:order_item_id", async (req, res) => {
-    const { order_item_id } = req.params;
-    const {
-        pictureDataId,
-        paperTypeId,
-        frameId,
-        orderId,
-        orderItemPrice,
-        priceSaved,
-        amount,
-    } = req.body;
+  const { order_item_id } = req.params;
+  const requestBody = filterBody(req.body);
 
-    const orderItemToEdit = await orderItems.findByPk(order_item_id).catch((error) => {
-        console.log(error);
-    });
-
+  try {
+    const orderItemToEdit = await OrderItem.findByPk(order_item_id);
     if (orderItemToEdit) {
-        const result = await orderItemToEdit.update({
-            pictureDataId,
-            paperTypeId,
-            frameId,
-            orderId,
-            orderItemPrice,
-            priceSaved,
-            amount,
-        }).catch((error) => {
-            console.log(error);
-        });
-
-        res.send({ order: result });
+      const updatedOrderItem = await orderItemToEdit.update(requestBody);
+      res.status(200).send(updatedOrderItem);
     } else {
-        res.status(404).send({ error: 404, message: "Order item not found." });
+      sendErrorResponse(res, "Order item not found.", 404);
     }
+  } catch (error) {
+    sendErrorResponse(res, "Unable to update order item.", 500, error);
+  }
 });
 
 // DELETE an order_item
 router.delete("/:order_item_id", async (req, res) => {
-    const { order_item_id } = req.params;
-    
-    const orderItemToDelete = await orderItems.findByPk(order_item_id).catch((error) => {
-        console.log(error);
-    });
+  const { order_item_id } = req.params;
+
+  try {
+    const orderItemToDelete = await OrderItem.findByPk(order_item_id);
 
     if (orderItemToDelete) {
-        await orderItemToDelete.destroy().catch((error) => {
-            console.log(error);
-        });
-
-        res.status(201).send({ message: "success" });
+      await orderItemToDelete.destroy();
+      res.status(200).send(orderItemToDelete);
     } else {
-        res.status(404).send({ error: 404, message: "Order item not found." });
+      sendErrorResponse(res, "Order item not found.", 404);
     }
+  } catch (error) {
+    sendErrorResponse(res, "Unable to update order item.", 500, error);
+  }
 });
+
+const filterBody = (body: {
+  pictureDataId: any;
+  paperTypeId: any;
+  frameId: any;
+  orderId: any;
+  orderItemPrice: any;
+  priceSaved: any;
+  amount: any;
+}) => {
+  const {
+    pictureDataId,
+    paperTypeId,
+    frameId,
+    orderId,
+    orderItemPrice,
+    priceSaved,
+    amount,
+  } = body;
+  return {
+    pictureDataId,
+    paperTypeId,
+    frameId,
+    orderId,
+    orderItemPrice,
+    priceSaved,
+    amount,
+  };
+};
 
 export default router;
