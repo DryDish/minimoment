e7972793f3468fd130cfad3118e8a15967089223
CristiPV <cristi.valentin29@gmail.com>
2022-05-18 10:19:43 +0200

    add subscription type nested document

--- src/models/mongo/subscription-type.ts
+++ src/models/mongo/subscription-type.ts
@@ -4,13 +4,13 @@ import {
   DECIMAL_15_2_MIN,
 } from "../../config/constants.config";
 
-interface SubscriptionTypeInterface {
+export interface SubscriptionTypeInterface {
   name: string;
   monthlyPrice: number;
   imageAmount: number;
 }
 
-const subscriptionTypeSchema = new Schema<SubscriptionTypeInterface>({
+export const subscriptionTypeSchema = new Schema<SubscriptionTypeInterface>({
   name: { type: String, maxlength: 45, unique: true, required: true },
   monthlyPrice: {
     type: Schema.Types.Decimal128,
--- src/models/mongo/subscription.ts
+++ src/models/mongo/subscription.ts
@@ -1,13 +1,19 @@
 import { Schema, model } from "mongoose";
+import {
+  SubscriptionTypeInterface,
+  subscriptionTypeSchema,
+} from "./subscription-type";
 
 interface SubscriptionInterface {
   startsAt: Date;
   endsAt: Date;
+  subscriptionType: SubscriptionInterface;
 }
 
 const subscriptionSchema = new Schema<SubscriptionInterface>({
   startsAt: { type: Schema.Types.Date, required: true },
   endsAt: { type: Schema.Types.Date, required: true },
+  subscriptionType: { type: subscriptionTypeSchema, required: true },
 });
 
 export const Subscription = model<SubscriptionInterface>(
