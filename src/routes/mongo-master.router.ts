9fd3bd9af406c4d74801e5b1110fa935bf71f3d7
CristiPV <cristi.valentin29@gmail.com>
2022-05-20 16:30:57 +0200

    add status routes

--- src/routes/mongo-master.router.ts
+++ src/routes/mongo-master.router.ts
@@ -1,15 +1,17 @@
 import express from "express";
 
-import AccountingRouter from "./mongo/accounting.routes"
+import AccountingRouter from "./mongo/accounting.routes";
 import RoleRouter from "./mongo/role.routes";
-import SubscriptionTypeRouter from "./mongo/subscription-types.routes"
-import DiscountCodeRouter from "./mongo/discount-codes.routes"
+import SubscriptionTypeRouter from "./mongo/subscription-types.routes";
+import DiscountCodeRouter from "./mongo/discount-codes.routes";
 import PictureRouter from "./mongo/picures.routes";
+import StatusRouter from "./mongo/statuses.routes";
 
 const router = express.Router();
 
 router.use("/reports", AccountingRouter);
 router.use("/admin/roles", RoleRouter);
+router.use("/admin/statuses", StatusRouter);
 router.use("/subscriptions/types", SubscriptionTypeRouter);
 router.use("/discounts/codes", DiscountCodeRouter);
 router.use("/pictures", PictureRouter);
