7718f72deb7d91c52013ca2b55e993fc9d691394
DryDish <57399961+DryDish@users.noreply.github.com>
2022-05-27 17:13:31 +0200

    added status router

--- src/routes/neo-master.router.ts
+++ src/routes/neo-master.router.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import RoleRouter from "./neo4j/role.routes";
+import StatusRouter from './neo4j/status.routes'
 
 const router = express.Router();
 
 router.use("/admin/roles/", RoleRouter);
+router.use("/admin/statuses/", StatusRouter);
 
 export default router;
