51f3fafc6363f7588467b1a1e5b9c8d56b02824f
CristiPV <cristi.valentin29@gmail.com>
2022-05-27 12:02:18 +0200

    fix neo4j config

--- src/config/neo4j.config.ts
+++ src/config/neo4j.config.ts
@@ -1,11 +1,11 @@
 import Neode from "neode";
 
 // Constants
-const PROTOCOL = process.env.MONGO_DB_HOST || "neo4j";
-const HOST = process.env.MONGO_DB_HOST || "localhost";
-const USER = process.env.MONGO_DB_USER || "neo4j";
-const PASSWORD = process.env.MONGO_DB_PASSWORD || "";
-const DATABASE = process.env.MONGO_DB_DATABASE || "neo4j";
+const PROTOCOL = process.env.NEO4J_DB_PROTOCOL || "neo4j";
+const HOST = process.env.NEO4J_DB_HOST || "localhost";
+const USER = process.env.NEO4J_DB_USER || "neo4j";
+const PASSWORD = process.env.NEO4J_DB_PASSWORD || "";
+const DATABASE = process.env.NEO4J_DB_DATABASE || "neo4j";
 
 export const instance = new Neode(`${PROTOCOL}://${HOST}:7687`, USER, PASSWORD, false, DATABASE);
 
