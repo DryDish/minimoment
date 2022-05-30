bdecd3035e463763ed0604f7c0b96fac87ae32bd
CristiPV <cristi.valentin29@gmail.com>
2022-05-30 19:36:35 +0200

    add mongoose deployment config

--- src/config/mongoose.config.ts
+++ src/config/mongoose.config.ts
@@ -7,10 +7,16 @@ const USER = process.env.MONGO_DB_USER || "";
 const PASSWORD = process.env.MONGO_DB_PASSWORD || "";
 const DATABASE = process.env.MONGO_DB_DATABASE || "";
 const AUTH_DATABASE = process.env.MONGO_DB_AUTH_DATABASE || "";
+const USE_ATLAS = process.env.MONGO_DB_USE_ATLAS || false;
+aasdad
+const getConnectionString = (): string => {
+  if (USE_ATLAS) {
+    return `mongodb+srv://${USER}:${PASSWORD}@${HOST}/${DATABASE}?authSource=${AUTH_DATABASE}&retryWrites=true&w=majority`;
+  }
+  return `mongodb://${USER}:${PASSWORD}@${HOST}:27017/${DATABASE}?authSource=${AUTH_DATABASE}`;
+};
 
 // Connect to the db
-mongoose.connect(
-  `mongodb://${USER}:${PASSWORD}@${HOST}:27017/${DATABASE}?authSource=${AUTH_DATABASE}`
-);
+mongoose.connect(getConnectionString());
 
 populate();
